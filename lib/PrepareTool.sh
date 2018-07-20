1ff00d2d96cf0e7189d6fe7f72264c0cd74c9bc8
gsshch <35591022+gsshch@users.noreply.github.com>
2018-07-20 16:31:32 +0800

    Update PrepareTool.sh

--- lib/PrepareTool.sh
+++ lib/PrepareTool.sh
@@ -7,8 +7,8 @@ if ! hash apt-get 2>/dev/null; then
     exit 1
 fi
 
-apt-get -y --no-install-recommends --fix-missing install \
-    bsdtar mtools u-boot-tools pv bc \
-    gcc automake make \
-    lib32z1 lib32z1-dev qemu-user-static \
-    dosfstools figlet device-tree-compiler debootstrap
+#apt-get -y --no-install-recommends --fix-missing install \
+#    bsdtar mtools u-boot-tools pv bc \
+#    gcc automake make \
+#    lib32z1 lib32z1-dev qemu-user-static \
+#   dosfstools figlet device-tree-compiler debootstrap
